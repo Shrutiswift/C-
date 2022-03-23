9092d1d525f8858fac4cd6420564af6051baf795
Shruti <shrutiswift@gmail.com>
2022-03-24 00:09:49 +0530

    Constructor

---
+++ Unit-1/Practicals.cpp/Practical11.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+class area {
+    int length, breadth, a;
+    public:
+    area()
+    {
+        length = 10;
+        breadth = 27;
+        cout << "Name: Shruti Sharma\nUID: 21BCS9445" << "\n";
+        cout << "Length = "<< length <<"\n";
+        cout << "Breadth = " << breadth << "\n";
+    }
+    area(int x, int y)
+    {
+        length = x;
+        breadth = y;
+    }
+    void calc();
+    void print();
+    ~area();
+
+};
+    void area::calc()
+    {
+        a = length * breadth;
+    }
+    void area::print()
+    {
+        cout << "Area = " << a << "\n";    
+    }
+    area::~area()
+    {
+        cout << "Object is being deleted"<< "\n";
+    }
+    int main()
+    {
+        int length, breadth;
+        area z1;
+        z1.calc();
+        z1.print();
+        cout << "Enter the length and breadth for parameterized constructor:" << "\n";
+        cin>>length>>breadth;
+        area z2(length, breadth);
+        z2.calc();
+        z2.print();
+        return 0;
+    }
