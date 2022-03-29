0cd7387712e6051995d8a833e26fcaaec2f43dbc
Shruti <shrutiswift@gmail.com>
2022-03-29 16:14:13 +0530

    Practicals

--- Unit-1/Practicals.cpp/Practical11.cpp
+++ Unit-1/Practicals.cpp/Practical11.cpp
@@ -1,13 +1,2 @@
 #include <iostream>
 using namespace std;
-class staff 
-{
-    protected:
-        char name[20];
-        int code;
-    public:
-        void getstaff(void)
-        {
-        cout << "Name: ";
-        }
-};
--- Unit-1/Practicals.cpp/Prcatical14.cpp
+++ Unit-1/Practicals.cpp/Prcatical14.cpp
@@ -28,6 +28,35 @@ class test : virtual public student {
         cout << "b\n" << b;
     }
 };
-class sports: virtual public student {
-    
+class sports: virtual public student{
+public:
+    int score;
+    void getscore() {
+        cout << "Score :";
+        cin >> score;
+    }
+    void putscore() {
+        cout << "Sports score: ";
+        cin >> score;
+    }
+
 };
+class result: public test, public sports {
+    int total;
+    public:
+    void display() {
+        total = a + b + score;
+        putnumber();
+        putscore();
+        putmarks();
+        cout << "Total Score:\n";
+    }
+};
+int main() {
+    result object;
+    object.getnumber();
+    object.getmarks();
+    object.getscore();
+    object.display();
+    return 0;
+}
