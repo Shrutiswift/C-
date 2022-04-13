03a979f22cee26a101572a11be6147348cef314f
Shruti <shrutiswift@gmail.com>
2022-04-13 22:51:25 +0530

    practice

--- Unit-1/Practice.cpp/scope.cpp
+++ Unit-1/Practice.cpp/scope.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int variable = 10;
 int main() {
     int variable = 8;
-    cout << "Value of global variable is: " << :: variable ;
-    cout << "\nValue of local variable is: "<< variable;
+    cout << "global variable is: " << :: variable ;
+    cout << "\nlocal variable is: "<< variable;
     return 0;
 }
