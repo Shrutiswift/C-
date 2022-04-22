f264cf397e5af189b374e03f9204f3b938f2876a
Shruti <shrutiswift@gmail.com>
2022-04-22 22:49:04 +0530

    exception

---
+++ Unit-1/Practice.cpp/Exception.cpp
@@ -0,0 +1,23 @@
+#include "iostream"
+using namespace std;
+
+int main() {
+    int numerator, denominator, result;
+    cout << "Enter numerator and denominator: " << endl;
+    cin >> numerator >> denominator;
+    try 
+    {
+        if(denominator == 0)
+        {
+            throw denominator;
+        }
+        int result = numerator/denominator;
+    }
+    catch (int ex)
+    {
+        cout << "Divide by zero is not allowed- "<< ex;
+    }
+    cout << " Division is: " << result;
+
+    return 0;
+}
