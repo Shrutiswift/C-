19306847cf171499e67f79ae70e0d50b572aa58f
Shruti <shrutiswift@gmail.com>
2022-05-02 15:24:39 +0530

    File

--- Unit-1/Practice.cpp/file.cpp
+++ Unit-1/Practice.cpp/file.cpp
@@ -1,17 +1,20 @@
 #include "iostream"
+#include "string"
 #include "fstream"
 using namespace std;
 int main(void) {
-    char name[20];
-    int age;
+    string name;
+    string age;
     cin >> name >> age;
+    ifstream fin ("ABC");
+    name.append(name);
+    age.append(age);
     ofstream fout ("ABC");
     fout << name << "\n" << age;
     fout.close();
-    ifstream fin ("ABC");
-    fin >> name;
-    cout << name <<endl;
-    fin >> age;
-    cout << age;
+    // fin >> name;
+    // cout << name <<endl;
+    // fin >> age;
+    // cout << age;
     fin.close();
 }
