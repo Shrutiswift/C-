ae4aa77c2437617f082bbf2d325add1d374004f0
Shruti <shrutiswift@gmail.com>
2022-05-09 13:57:54 +0530

    Pointers

--- Unit-1/Practice.cpp/Handling.cpp
+++ Unit-1/Practice.cpp/Handling.cpp
@@ -1,6 +1,12 @@
-#include "iostream"
+#include <iostream>
 using namespace std;
 
-class student {
 
-};
+int main() {
+    int a = 9;
+    int *b = &a;
+    int *h= b;
+
+    cout << h << endl;
+    return 0;
+}
