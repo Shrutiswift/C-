2dc1df23bf143168382a4594a98f5cb017585c75
Shruti <shrutiswift@gmail.com>
2022-06-06 20:36:26 +0530

    Practice

--- Unit-1/Practicals.cpp/mst.cpp
+++ Unit-1/Practicals.cpp/mst.cpp
@@ -2,35 +2,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void handy(int test)
-{
-    try
-    {
-        if (test == 0) throw test;
-        if (test == 1) throw 'a';
-        if (test == 2) throw 123.23;
-        else throw "zoro";
-    }
-
-    catch (int i)
-    {
-        cout << "Caught: " << i << endl;
-    }
-
-    catch (...)
-    {
-        cout << "Caught All Other Exceptions!" << endl;
-    }
-}
-int main()
-{
-    cout << "<===== START =====>" << endl;
-    cout << "NIKHIL\nUID- 21BCS9434\n";
-    int input;
-    for(int i=0;i<4;i++){
-    cin>>input;
-    handy(input);
-    }
-    cout << "<===== END ======>";
+class empty{};
+int main(){
+    cout << sizeof(empty);
     return 0;
 }
